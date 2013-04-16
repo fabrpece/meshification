ddbcdddfcf14079a44b1caccdc518c559cc8af0d
Paolo Simone Gasparello <djgaspa@gmail.com>
2013-04-16 12:46:57 +0100

    Allow multiple static models.

--- receiver/xvr_receiver.cpp
+++ receiver/xvr_receiver.cpp
@@ -1,10 +1,11 @@
 #include <memory>
+#include <list>
 #include "xvr_receiver.h"
 #include "Receiver.hpp"
 #include "StaticModel.hpp"
 
 static std::unique_ptr<Receiver> p;
-static std::unique_ptr<StaticModel> p_static;
+static std::list<StaticModel> static_models;
 
 extern "C" {
 
@@ -12,25 +13,26 @@ void xvr_receiver_init()
 {
     Receiver::init();
     p.reset(new Receiver);
-    p_static.reset(new StaticModel);
     p->start();
 }
 
 void xvr_receiver_draw()
 {
     p->draw();
-    p_static->draw();
+    for (const auto& m : static_models)
+        m.draw();
 }
 
 void xvr_receiver_destroy()
 {
     p.reset();
-    p_static.reset();
+    static_models.clear();
 }
 
 void xvr_receiver_load_static(const char* fname)
 {
-    p_static->load(fname);
+    static_models.emplace_back();
+    static_models.back().load(fname);
 }
 
 void xvr_receiver_translate(const int i, const double x, const double y, const double z)
